e49b6f2d8405ec0db71470d423abc444b297dda5
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-05-10 13:38:18 +0900

    add react-native-web settings

--- index.web.js
+++ index.web.js
@@ -2,3 +2,8 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 
 AppRegistry.registerComponent('cross_platform_sample', () => App);
+
+AppRegistry.runApplication('cross_platform_sample', {
+  initialProps: {},
+  rootTag: document.getElementById('root'),
+});
